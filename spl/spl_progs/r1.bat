a2144619ac626156b6175af7bd16fab6fa39fcae
akshit-kumar-07 <132078941+akshit-kumar-07@users.noreply.github.com>
2023-06-05 17:58:39 +0530

    Add files via upload

---
+++ spl/spl_progs/r1.bat
@@ -0,0 +1,7 @@
+load --init /home/akshit/myexpos/expl/expl_progs/squares.xsm
+load --int=10 ../spl/spl_progs/haltprog.xsm
+load --exhandler ../spl/progs/haltprog.xsm
+load --int=timer /home/akshit/myexpos/spl/spl_progs/sample_timer.xsm
+load --int=7 /home/akshit/myexpos/spl/spl_progs/sample_int7.xsm
+load --os /home/akshit/myexpos/spl/spl_progs/os_handler.xsm
+
